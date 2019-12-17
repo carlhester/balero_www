3a3f0a9bdd8be91e09ac7d85f1724ad7130f5391
Carl Hester <crucialcarl@gmail.com>
2019-12-17 02:02:08 +0000

    minor changes

--- main.go
+++ main.go
@@ -9,13 +9,16 @@ import "sort"
 import "strconv"
 import "balero_www/json2struct"
 
+// trains is a slice of ATrain objects
 var trains = []ATrain{}
 
+// ATrain is a single train
 type ATrain struct {
 	Train   string
 	Minutes int
 }
 
+// Minutes is returned from api as a string, convert to int
 func convertStrToInt(input string) int {
 	if input == "Leaving" {
 		input = "0"
@@ -27,44 +30,46 @@ func convertStrToInt(input string) int {
 	return i
 }
 
+// The data on the page comes from this struct
 type PageData struct {
-	Train []ATrain
+	Trains []ATrain
 }
 
-func findATrain() {
-
+// fetch the data from the api
+func fetchTrains() []ATrain {
+	var fetchedTrains = []ATrain{}
 	url := "http://api.bart.gov/api/etd.aspx?cmd=etd&orig=mont&key=MW9S-E7SL-26DU-VV8V&dir=n&json=y"
 	resp, err := http.Get(url)
 
 	if err != nil {
 		log.Panic(err)
 	}
-	data, err := ioutil.ReadAll(resp.Body)
+
+	trainData, err := ioutil.ReadAll(resp.Body)
 	defer resp.Body.Close()
 
 	if err != nil {
 		log.Panic(err)
 	}
 
-	usableData := json2struct.RawDataIntoDataStruct(data)
+	usableData := json2struct.RawDataIntoDataStruct(trainData)
 
 	for _, train := range usableData.Root.Station[0].Etd {
 		for _, est := range train.Est {
 			minutes := convertStrToInt(est.Minutes)
-			trains = append(trains, ATrain{train.Abbreviation, minutes})
-			//fmt.Println(train.Abbreviation, minutes)
+			fetchedTrains = append(fetchedTrains, ATrain{train.Abbreviation, minutes})
 		}
 	}
-	trains = sortSlice(trains)
+	return fetchedTrains
 
 }
 func serveUI(rw http.ResponseWriter, r *http.Request) {
 	start := time.Now()
-	trains = trains[:0]
-	findATrain()
+	trains = fetchTrains()
+	trains = sortSlice(trains)
 	tmpl, err := template.ParseFiles("templates/index.html")
 	page := PageData{
-		Train: trains,
+		Trains: trains,
 	}
 	if err = tmpl.Execute(rw, page); err != nil {
 		log.Panic("Failed to write template", err)
